0b471349457217b133c928b422331957648b988a
Nathan Conlon <nathanec@gmail.com>
2024-06-07 10:01:38 -0500

    created from sinatra-starter

---
+++ Gemfile
@@ -0,0 +1,23 @@
+ruby '3.3.1'
+source 'https://rubygems.org'
+
+gem 'oj'
+
+gem 'datamapper'
+gem 'sassc'
+gem 'sinatra'
+gem 'sinatra-js'
+gem 'slim'
+gem 'tilt'
+gem 'thin'
+gem 'token_phrase'
+
+group :development do
+  gem 'sinatra-reloader'
+  gem 'pry'
+end
+
+group :production do
+  gem 'puma'
+  gem 'librato-rack'
+end
---
+++ app.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal = true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/js'
+require 'slim'
+require 'tilt'
+require 'sassc'
+
+# Server Settings
+set server 'thin', connections: []
+set :bind, '0.0.0.0'
+set :port, '4567'
+
+get '/' do
+  slim :index
+end
